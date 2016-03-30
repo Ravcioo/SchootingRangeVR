dab9ce41831dc6a1fc1feefa60d9c2bb77861986
Rafal <gat.rafal@gmail.com>
2016-03-30 21:02:03 +0200

    added very simple CustomCollisionSystem, now working only on unified spheres

---
+++ SchootingRangeVR/Assets/Scripts/CustomCollisionSystem/CollisionManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class CollisionManager : MonoBehaviour
+{
+
+    private static List<CustomSphereCollider> colliders;
+    public static CollisionManager Instance = null;                                     
+
+   
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    
+    public void AddCollider(CustomSphereCollider collider)
+    {
+        if (colliders == null)
+        {
+            colliders = new List<CustomSphereCollider>();
+        }
+        colliders.Add(collider);
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < colliders.Count - 1; i++)
+        {
+            for (int j = i + 1; j < colliders.Count; j++)
+            {
+                colliders[i].CheckCollision(colliders[j]);
+            }
+        }
+    }
+
+
+
+}
+    
---
+++ SchootingRangeVR/Assets/Scripts/CustomCollisionSystem/CustomSphereCollider.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomSphereCollider : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool drawCollider = false;
+    public float Radius { get { return GetMaxScale(); } set { SetLossyScaleTo(value); } }
+    void Awake()
+    {
+        CollisionManager.Instance.AddCollider(this);
+    }
+
+    private float GetMaxScale()
+    {
+        return Mathf.Max(transform.lossyScale.x / 2, transform.lossyScale.y / 2, transform.lossyScale.z / 2);
+    }
+    private void SetLossyScaleTo(float value)
+    {
+        Transform parent = transform.parent;
+        transform.parent = null;
+        transform.localScale = new Vector3(value*2, value*2, value*2);
+        transform.parent = parent;
+    }
+
+    public void CheckCollision(CustomSphereCollider collider)
+    {
+
+        if (Vector3.Distance(transform.position, collider.transform.position) < Radius + collider.Radius)
+        {
+            Debug.Log("Collision");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (drawCollider)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(transform.position, GetMaxScale());
+        }
+    }
+
+
+
+}
