f5e8f3d4ae7bcd20db42cebc399bd615d3ade92b
BluuTM <nav22@interia.pl>
2016-04-02 21:53:46 +0200

    added gun and enviro

---
+++ SchootingRangeVR/Assets/Scripts/Gun/GunFollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunFollowCamera : MonoBehaviour {
+
+    [SerializeField]
+    private Camera targetCamera;
+
+    Vector3 rotationOffset;
+
+    void Start()
+    {
+        rotationOffset = targetCamera.transform.rotation.eulerAngles - transform.rotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        Vector3 targetRotation = targetCamera.transform.rotation.eulerAngles;
+        targetRotation.x *= -1;
+        targetRotation += rotationOffset;
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation,  Quaternion.Euler(targetRotation), 100 * Time.deltaTime);
+    }
+}
