f612b437c74a8ae051823cdee52f6d75cb2fe9ec
Rafal <gat.rafal@gmail.com>
2016-04-06 19:17:16 +0200

    forgotten :D

---
+++ SchootingRangeVR/Assets/Scripts/Gun/Gun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+    [Header("Table")]
+    [SerializeField]
+    private Transform tableHolder;
+    [SerializeField]
+    private Vector3 tablePosition;
+    [SerializeField]
+    private Vector3 tableRotation;
+    [Header("Hand")]
+    [SerializeField]
+    private Transform handHolder;
+    [SerializeField]
+    private Vector3 handPosition;
+    [SerializeField]
+    private Vector3 handRotation;
+    
+
+
+    public void Equip()
+    {
+        transform.parent = handHolder;
+        transform.localPosition = handPosition;
+        transform.localRotation= Quaternion.Euler(handRotation);
+    }
+
+    public void PutDown()
+    {
+        transform.parent = tableHolder;
+        transform.localPosition= tablePosition;
+        transform.localRotation = Quaternion.Euler(tableRotation);
+    }
+}
